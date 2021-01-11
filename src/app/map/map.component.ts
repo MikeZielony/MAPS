865c857ad5669989c81a24cb4a82e721aaf76ee5
MikeZielony <51535787+MikeZielony@users.noreply.github.com>
2021-01-11 14:11:55 +0100

    Add

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 @Component({
   selector: 'app-map',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  map;
+  markerIcon = {
+    icon: L.icon({
+      iconSize: [25, 41],
+      iconAnchor: [10, 41],
+      popupAnchor: [2, -40],
+      // specify the path here
+      iconUrl: 'https://unpkg.com/leaflet@1.5.1/dist/images/marker-icon.png',
+      shadowUrl: 'https://unpkg.com/leaflet@1.5.1/dist/images/marker-shadow.png'
+    })
+  };
 
-  ngOnInit(): void {
-  }
+  ngOnInit() {
+    if (!navigator.geolocation) {
+      console.log('location is not supported');
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+      const coords = position.coords;
+      const latLong = [coords.latitude, coords.longitude];
+      console.log(
+          `lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`
+      );
 
+      this.map = L.map('map').setView(latLong, 19);
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+            '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(this.map);
+      L.marker([latLong[0], latLong[1]], this.markerIcon).addTo(this.map).addGroup;
+
+      this.map.on('click', e => {
+        console.log(e.latlng); // get the coordinates
+        L.marker([e.latlng.lat, e.latlng.lng], this.markerIcon).addTo(this.map); // add the marker onclick
+      });
+    })}
 }
